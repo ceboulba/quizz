ad960cea61de06734aec7db3f04dcd4fc401679e
unknown <antoine@archipicture.fr>
2018-04-27 09:51:11 +0200

    from-shadow

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import "./App.css";
+// import "./App.css";
 
 // STYLES //
 const defaultStyle = {
@@ -12,112 +12,106 @@ const defaultStyle = {
 };
 const typos = {
   fontFamily: "Roboto",
-  color: "#fff",
-  
+  color: "#000"
 };
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state.questionsList = this.props.data
-  }
-  state = {
-    random: 0,
-    points: 0
-  };
-  
-  
+    this.state = {
+      titre: this.props.titre,
+      data: this.props.data,
+      questionsList: this.props.data,
+      random: 0,
+      points: 0
+    };
 
-  ///   <FUNCTIONS>   ///
-  makeRandomNumber = r => {
-    const temp = r;
-    const random = Math.floor(Math.random() * this.props.data.length);
-    return random !== temp ? random : 1;
-  };
-  
-  compteur = () => {
-    this.setState({ points: this.state.points + 1 });
-  };
-  
-  algo = reponse => {
-    (() => {
-      this.props.data[this.state.random].good === reponse
-      ? this.setState(this.compteur())
-      : alert("|||  LOOSE |||");
-    })();
-    this.state.points === 4
-    ? (() => {
-      alert("VICTOIRE");
-      this.setState({ points: 0 });
-    })()
-    : (() => {
-      console.log(`points:  ${this.state.points + 1} `);
-    })();
-  };
-  
-  print = data => {
-    console.log(data);
-  };
-  
-  componentDidMount() {
-    this.print(this.props);
+    console.log(this.props);
+
+    this.print = data => {
+      data.map((d, i) => console.log(`question n ${i} : ${d}`));
+    };
+
+    this.makeRandomNumber = r => {
+      const temp = r;
+      const random = Math.floor(Math.random() * this.props.data.length);
+      return random !== temp ? random : 1;
+    };
+
+    this.compteur = () => {
+      this.setState({ points: this.state.points + 1 });
+    };
+
+    this.algo = reponse => {
+      (() => {
+        this.props.data[this.state.random].good === reponse
+          ? this.setState(this.compteur())
+          : alert("|||  LOOSE |||");
+      })();
+      this.state.points === 4
+        ? (() => {
+            alert("VICTOIRE");
+            this.setState({ points: 0 });
+          })()
+        : (() => {
+            console.log(`points:  ${this.state.points + 1} `);
+          })();
+    };
   }
 
-  ///   <RENDER>    ///
   render() {
     return (
       <div className="App" style={typos}>
-        <h1 style={defaultStyle} className="App-title border">
+        <h1 className="title is-1 has-text-white">
           {this.props.titre}
         </h1>
+        <p>Question n° {this.state.random}</p>
+        <h3 className="subtitle is-3 has-text-white is-uppercase">{this.props.data[this.state.random].question}</h3>
         <div>
-          <p>Question n° {this.state.random}</p>
-          <h3>{this.props.data[this.state.random].question}</h3>
-          <div>
-            <p>{this.props.data[this.state.random].reponse}</p>
-          </div>
-          <span>
-            <button
-              style={defaultStyle}
-              id="btn"
-              onClick={() => {
-                this.algo(0);
-                this.setState({
-                  random: this.makeRandomNumber(this.state.random)
-                });
-              }}
-            >
-              0
-            </button>
-          </span>
-
-          <span>
-            <button
-              style={defaultStyle}
-              id="btn"
-              onClick={() => {
-                this.algo(1);
-                this.setState({
-                  random: this.makeRandomNumber(this.state.random)
-                });
-              }}
-            >
-              1
-            </button>
-          </span>
+          <p className="subtitle is-4 has-text-white">{this.props.data[this.state.random].reponse}</p>
         </div>
+        <span>
+          <button
+            className="button"
+            id="btn"
+            onClick={() => {
+              this.algo(0);
+              this.setState({
+                random: this.makeRandomNumber(this.state.random)
+              });
+            }}
+          >
+            0
+          </button>
+        </span>
+        <span>
+          <button
+            className="button"
+            id="btn"
+            onClick={() => {
+              this.algo(1);
+              this.setState({
+                random: this.makeRandomNumber(this.state.random)
+              });
+            }}
+          >
+            1
+          </button>
+        </span>
+        <div>
+
         <button
-          style={typos}
+          className="button"
           onClick={(actionOne, actionTwo) => {
             actionOne = this.setState({
               random: this.makeRandomNumber(this.state.random)
             });
             actionTwo = console.log("CA MARCHE !");
           }}
-        >
+          >
           New Question
         </button>
-
+          </div>
         <h3 style={defaultStyle}>points = {this.state.points}</h3>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+// import "./index.css";
 import App from "./App";
 import data from "./data.json"
 import registerServiceWorker from "./registerServiceWorker";
